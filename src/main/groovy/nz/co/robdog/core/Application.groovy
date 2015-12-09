5bd9b2ebb5747e0d09edd1c355ed023c05a19085
Robert Read @progdroog <robert.simon.read@gmail.com>
2015-12-09 21:56:31 +1300

    Initial set-up of Spring, project, Maven, integration to github etc.

---
+++ src/main/groovy/nz/co/robdog/core/Application.groovy
@@ -0,0 +1,15 @@
+package nz.co.robdog.core
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ * Created by Robert Read <robert.simon.read> on 9/12/2015.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args)
+    }
+
+}
